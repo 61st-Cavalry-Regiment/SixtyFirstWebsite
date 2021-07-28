429a5b738ea0084dfb2861974ac2858a6c432067
Jacob Oxley <jdoxley6@gmail.com>
2021-07-27 22:09:43 -0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+
+import { Container, Heading } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <Container centerContent>
+      <Heading size="xl">Welcome to the 61st Cavalry Regiment (Arma 3)</Heading>
+    </Container>
+  )
+}
