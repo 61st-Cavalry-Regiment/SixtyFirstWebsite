0897d7ecbe2120a4c405de946e621fbf43cd9163
Jacob Oxley <jdoxley6@gmail.com>
2021-11-25 01:48:20 -0800

    mb fix

---
+++ module.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        CDN: string
+        NEXT_PUBLIC_CDN: string
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+        NEXT_PUBLIC_SUPABASE_URL: string
+    }
+}
--- src/pages/unitphotos.tsx
+++ src/pages/unitphotos.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-css-tags */
 import Head from 'next/head'
 import { parseUrl } from '../components/Image'
 import Image from 'next/image'
